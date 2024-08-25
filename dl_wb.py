a5c6fbc76bd01a8d477829335fd92cb2b825b15d
zehr0s <zehr0.s@protonmail.com>
2024-08-25 02:48:37 -0600

    Add threading

--- dl_wb.py
+++ dl_wb.py
@@ -7,12 +7,14 @@
 from modules import Crawler
 from modules import Downloader
 from modules.Tools import create_gallery
+from datetime import datetime
 import os
 
 # Target
-base = 'https://luminousscans.com'
+base = 'https://radiantscans.com/'
 search = base + '/?s=wind+breaker'
 current_title = 'windbreaker'
+start_time = datetime.now()
 
 # Filters
 link_filter = {
@@ -54,19 +56,42 @@ total = len(craw.pages)
 chapters = []
 titles = []
 for i, chapter in enumerate(craw.pages):
-    print('Reading chapters: {}/{}'.format(i+1, total))
+    print('Reading chapters: {}/{} | [{}]'.format(i+1, total, chapter))
     titles.append(chapter)
     chapters.append( craw.search(chapter, image_filter, use_base_prefix=False) )
 os.system('clear')
 
 # Download images by chapter
 dl = Downloader(prefix = 'Pages')
+
+#def download_chapter(info):
+#    title, chapter = info
+#    if len(chapter) == 0:
+#        return None  # Skip empty chapters
+#    print('Downloading chapter: [{}]'.format(title))
+#    dl.download(chapter, 'manga/{}/{}'.format(current_title, title.split('/')[-2].split('-')[-1]))
+#    return title
+#
+#with ThreadPoolExecutor(max_workers=10) as executor:
+#    futures = {executor.submit(download_chapter, info): info for info in zip(titles, chapters)}
+#    for i, future in enumerate(as_completed(futures)):
+#        title = future.result()
+#        if title is not None:
+#            print('Chapters: {}/{} - {:.2f}% - [{}]'.format(i + 1, len(chapters), (i + 1) / len(chapters) * 100, title))
+#            os.system('clear')
+
 for i, info in enumerate(zip(titles, chapters)):
     title, chapter = info
     if len(chapter) == 0:
         continue
-    print('Chapters: {}/{} - {:.2f} %'.format( i+1, len(chapters), i/len(chapters)*100 ))
-    dl.download(chapter, 'manga/{}/{}'.format(current_title, title.split('/')[-2].split('-')[-1]))
+    print('Chapters: {}/{} - {:.2f}% - [{}]'.format( i+1, len(chapters), i/len(chapters)*100, title))
+    download_path = 'manga/{}/{}'.format(current_title, title.split('/')[-2].split('-')[-1])
+    os.makedirs(download_path, exist_ok=True)
+    dl.download(
+        chapter,
+        workers=4,
+        download_path=download_path
+    )
     os.system('clear')
 
 # Create scrollable gallery
@@ -74,3 +99,7 @@ create_gallery('manga/{}'.format(current_title), type='scrollable')
 
 # TODO: Create gallery and update menu
 # create_menu('manga/{}'.format(current_title), type='scrollable')
+
+end_time = datetime.now()
+dt_s = (end_time - start_time).seconds
+print(f"[+] Downloading {len(chapters)} chapters took {dt_s/60:.2f} min")
--- modules/Downloader.py
+++ modules/Downloader.py
@@ -9,10 +9,12 @@ import requests
 import shutil
 import sys
 import os
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 class Downloader:
     # Class attributes
-    def __init__(self, prefix='Downloading', timeout=15):
+    def __init__(self, prefix='Downloading', timeout=60):
         self.timeout = timeout
         self.prefix = prefix
 
@@ -31,39 +33,91 @@ class Downloader:
         else:                       # success
             print('[*] {}: {}'.format(self.prefix, message))
 
-    def download(self, links, download_path='output_files'):
-        os.makedirs(download_path, exist_ok=True)
-        # Iterate pages
-        for i, img_url in enumerate(links):
-            # Request image
-            try:
-                r = requests.get(img_url, timeout=self.timeout)
-            except (requests.exceptions.Timeout, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
-                self.print_custom('Timeout in page url {}'.format(img_url), type=1)
-                # Remove incomplete download
-                shutil.rmtree('{}'.format(download_path))
-                continue
-            except KeyboardInterrupt:
-                self.print_custom('Keyboad Interrupt in {}'.format(img_url), type=1)
-                # Remove incomplete download
-                shutil.rmtree('{}'.format(download_path))
-                sys.exit(1)
-            except:
-                self.print_custom('Error in {}'.format(img_url), type=1)
-                # Remove incomplete download
-                shutil.rmtree('{}'.format(download_path))
-                continue
-            if not r.ok:
-                self.print_custom('Connection error in page url {}'.format(img_url), type=1)
-                # remove incomplete download
-                shutil.rmtree('{}'.format(download_path))
-                continue
 
-            # download page
-            with open('{}/{:02}.jpg'.format(download_path, i+1), 'wb') as f:
-                f.write(r.content)
+    def download_image(self, info):
+        i, img_url, download_path = info
+        start_time = datetime.now()
+        try:
+            r = requests.get(img_url, timeout=self.timeout)
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
+            self.print_custom('Timeout in page url {}'.format(img_url), type=1)
+            shutil.rmtree(download_path, ignore_errors=True)
+            return None
+        except KeyboardInterrupt:
+            self.print_custom('Keyboard Interrupt in {}'.format(img_url), type=1)
+            shutil.rmtree(download_path, ignore_errors=True)
+            sys.exit(1)
+        except Exception as e:
+            self.print_custom(f"Error in {img_url}: {str(e)}", type=1)
+            shutil.rmtree(download_path, ignore_errors=True)
+            return None
 
-            # update status
+        if not r.ok:
+            self.print_custom('Connection error in page url {}'.format(img_url), type=1)
+            shutil.rmtree(download_path, ignore_errors=True)
+            return None
+
+        # Save the image
+        image_path = f'{download_path}/{i+1:02}.jpg'
+        with open(image_path, 'wb') as f:
+            f.write(r.content)
+
+        end_time = datetime.now()
+        dt_s = (end_time - start_time).seconds
+
+        # Update status
+        return (i, dt_s, len(r.content))
+
+
+    def download(self, links, workers=4, download_path='output_files'):
+        # Use ThreadPoolExecutor to download images in parallel
+        with ThreadPoolExecutor(max_workers=workers) as executor:  # Adjust max_workers as needed
+            futures = {executor.submit(self.download_image, (i, img_url, download_path)): i for i, img_url in enumerate(links)}
+
             total = len(links)
-            self.print_custom('{:.2f} %'.format( (i+1)/total*100 ))
+            for i, future in enumerate(as_completed(futures)):
+                result = future.result()
+                if result is not None:
+                    idx, dt_s, content_length = result
+                    self.print_custom('{:.2f}% | {}s | {} bytes'.format((idx+1)/total*100, dt_s, content_length))
+                else:
+                    self.print_custom('Failed to download image {}/{}'.format(i+1, total))
+
+#        os.makedirs(download_path, exist_ok=True)
+#        # Iterate pages
+#        for i, img_url in enumerate(links):
+#            start_time = datetime.now()
+#            # Request image
+#            try:
+#                r = requests.get(img_url, timeout=self.timeout)
+#            except (requests.exceptions.Timeout, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
+#                self.print_custom('Timeout in page url {}'.format(img_url), type=1)
+#                # Remove incomplete download
+#                shutil.rmtree('{}'.format(download_path))
+#                continue
+#            except KeyboardInterrupt:
+#                self.print_custom('Keyboad Interrupt in {}'.format(img_url), type=1)
+#                # Remove incomplete download
+#                shutil.rmtree('{}'.format(download_path))
+#                sys.exit(1)
+#            except:
+#                self.print_custom('Error in {}'.format(img_url), type=1)
+#                # Remove incomplete download
+#                shutil.rmtree('{}'.format(download_path))
+#                continue
+#            if not r.ok:
+#                self.print_custom('Connection error in page url {}'.format(img_url), type=1)
+#                # remove incomplete download
+#                shutil.rmtree('{}'.format(download_path))
+#                continue
+#
+#            # download page
+#            with open('{}/{:02}.jpg'.format(download_path, i+1), 'wb') as f:
+#                f.write(r.content)
+#            end_time = datetime.now()
+#            dt_s = (end_time - start_time).seconds
+#
+#            # update status
+#            total = len(links)
+#            self.print_custom('{:.2f}% | {}s'.format( (i+1)/total*100, dt_s ))
 
